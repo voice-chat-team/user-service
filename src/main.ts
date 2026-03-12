9a7af19f36a95369b16447fe02c9a59ce76b0e71
TankistPro <nakol.nayk@mail.ru>
2026-03-12 22:56:14 +0300

    feat: create user-microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 50501,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
