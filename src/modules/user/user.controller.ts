88e73a884ea8c0e52d36fbe6b5a40971cfdd5d76
TankistPro <nakol.nayk@mail.ru>
2026-03-14 21:50:08 +0300

    feat: add user service contracts and implement user lookup methods

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,30 @@
 import { Controller } from '@nestjs/common';
 import { UserService } from './user.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import {
+  GetUserByEmail,
+  GetUserById,
+  GetUserByUsername,
+  USER_MESSAGE_PATTERNS,
+} from '@voice-chat/contracts';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @MessagePattern('user.getUser')
-  getUser() {
-    return this.userService.getUser();
+  @MessagePattern(USER_MESSAGE_PATTERNS.GET_BY_EMAIL)
+  getUserByEmail(@Payload() { email }: GetUserByEmail) {
+    console.log(email);
+    return this.userService.getUserByEmail(email);
+  }
+
+  @MessagePattern(USER_MESSAGE_PATTERNS.GET_BY_ID)
+  getUserByID(@Payload() { userId }: GetUserById) {
+    return this.userService.getUserById(userId.toString());
+  }
+
+  @MessagePattern(USER_MESSAGE_PATTERNS.GET_BY_USERNAME)
+  getUserByUsername(@Payload() { username }: GetUserByUsername) {
+    return this.userService.getUserByUsername(username);
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  getUser() {
-    return {
-      id: 1,
-      username: 'TankistPro',
-    };
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUserByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async getUserById(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async getUserByUsername(username: string) {
+    return this.prisma.user.findUnique({ where: { username } });
   }
 }
